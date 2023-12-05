015b764cf6767c691fe71f2912a5e244afd5f092
ZuKerNo1 <trankimvu2002@gmail.com>
2023-12-05 17:49:53 +0700

    done bottom navigator, add screens user, notify, message

--- App.js
+++ App.js
@@ -3,42 +3,15 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import { Login, Register, Welcome, Home } from './screens';
+import AuthNavigator from './navigation/AuthNavigator';
+
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
     return (
         <NavigationContainer>
-            <Stack.Navigator initialRouteName='Welcome'>
-                <Stack.Screen
-                    name='Welcome'
-                    component={Welcome}
-                    options={{
-                        headerShown: false,
-                    }}
-                />
-                <Stack.Screen
-                    name='Login'
-                    component={Login}
-                    options={{
-                        headerShown: false,
-                    }}
-                />
-                <Stack.Screen
-                    name='Register'
-                    component={Register}
-                    options={{
-                        headerShown: false,
-                    }}
-                />
-                <Stack.Screen
-                    name='Home'
-                    component={Home}
-                    options={{
-                        headerShown: false,
-                    }}
-                />
-            </Stack.Navigator>
+            <AuthNavigator/>
         </NavigationContainer>
     );
 }
---
+++ screens/User.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text, SafeAreaView } from 'react-native';
+
+const User = () => {
+    return (
+        <SafeAreaView>
+            <View>
+                <Text>User</Text>
+            </View>
+        </SafeAreaView>
+    );
+}
+
+export default User;
